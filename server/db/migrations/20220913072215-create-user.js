c0bb3be406be44cd63611e99ffb9b90d54076bc7
Shamil <shamil.chuchaev@gmail.com>
2022-09-13 13:45:08 +0500

    dbstart

---
+++ server/db/migrations/20220913072215-create-user.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      login: {
+        unique: true,
+        type: Sequelize.STRING
+      },
+      email: {
+        unique: true,
+        type: Sequelize.STRING
+      },
+      password: {
+        unique: true,
+        type: Sequelize.STRING
+      },
+      role: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ server/db/models/product.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+    this.hasOne(models.BasketProduct, { foreignKey: 'basket_id' });
+    this.belongsTo(models.Brand, { foreignKey: 'brand_id' });
+    this.belongsTo(models.Type, { foreignKey: 'type_id' });
+    this.hasMany(models.ProductInfo, { foreignKey: 'product_id' });
+    this.hasMany(models.Rating, { foreignKey: 'product_id' });
+  }
+  }
+  Product.init({
+    name: DataTypes.STRING,
+    price: DataTypes.INTEGER,
+    rating: DataTypes.INTEGER,
+    img: DataTypes.STRING,
+    type_id: DataTypes.INTEGER,
+    brand_id: DataTypes.INTEGER,
+    start_date: DataTypes.DATE,
+    end_date: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Product',
+  });
+  return Product;
+};
