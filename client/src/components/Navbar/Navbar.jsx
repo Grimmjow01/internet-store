42804349a335a56e36e1e1c0072b90db5b6bc090
Shamil <shamil.chuchaev@gmail.com>
2022-09-14 13:12:22 +0500

    Register

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  AppBar, Badge, Button, Toolbar, Typography,
+  AppBar, Badge, Button, Toolbar, Typography, Dialog
 } from '@mui/material';
 import { Stack } from '@mui/system';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
 import StoreRoundedIcon from '@mui/icons-material/StoreRounded';
 import { useNavigate } from 'react-router-dom';
+import Auth from '../Auth/Auth'
 
 function Navbar() {
   const navigate = useNavigate(); // или используй Navlink
+  const [open, setOpen] = useState(false);
+
+  const handClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <AppBar position="sticky">
       <Toolbar>
@@ -21,10 +31,13 @@ function Navbar() {
         <Button color="inherit" onClick={() => navigate('/contacts')}>
             Связатся с нами
           </Button>
-          <Button color="inherit" onClick={() => navigate('/login')}>
+          <Button color="inherit" onClick={handClickOpen}>
             <AccountCircleIcon fontSize="large" />
             Войти
           </Button>
+          <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+            <Auth />
+          </Dialog>
           <Button color="inherit" onClick={() => navigate('/basket')}>
             <Badge badgeContent={5} color="error">
               <ShoppingCartRoundedIcon fontSize="large" />
