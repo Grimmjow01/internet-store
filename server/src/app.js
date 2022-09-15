f4afe6a2dd64008b42f79e2699b670644da1ed21
Igor Polnik <ipolnik@yahoo.com>
2022-09-16 00:31:50 +0600

    corses-fix

--- server/src/app.js
+++ server/src/app.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3000/*',
     methods: ["GET", "POST"]
   }
 } );
